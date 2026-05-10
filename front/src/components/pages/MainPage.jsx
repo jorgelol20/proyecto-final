b365999fa0295d9872f8659c6d211f0fe93e15b6
jorgelol20 <94186347+jorgelol20@users.noreply.github.com>
2026-05-10 17:23:15 +0200

    Ranking

--- front/src/components/pages/MainPage.jsx
+++ front/src/components/pages/MainPage.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useContext, useEffect, useState } from "react";
 import './MainPage.css';
 import { useUser } from "../../hooks/useUser.js";
 import Banner from "../structure/Banner";
@@ -8,13 +8,17 @@ import { settingsContext } from "../../context/SettingsProvider.jsx";
 import SelectModifier from "../SelectModifier.jsx";
 import { useMatch } from "../../hooks/useMatch.js";
 import Match from "../Match.jsx";
+import UserShow from "../UserShow.jsx";
+import UserRanking from "../UserRanking.jsx";
 
 
 const MainPage = () => {
-    const { user, isLoading } = useUser()
+    const { user, isLoading, getRanking } = useUser()
     const { matches } = useMatch();
     const { startMusic, startButtonSound } = useContext(settingsContext)
     const navigate = useNavigate()
+
+    
     if (isLoading) {
         return (
             <Fragment>
@@ -39,10 +43,13 @@ const MainPage = () => {
                             : <></>
                     }
                 </div>
+                <div>
+                    <UserRanking/>
+                </div>
             </article>
             <article>
                 <div className="last-matches">
-                    <h1>Últimas partidas</h1>
+                    <h1>Últimas partidas ( Global )</h1>
                     <div className="match-history">
                         {matches?.map((match, index) => {
                             return <div key={crypto.randomUUID()} onClick={() => { navigate(`/partida/${match.id}`) }}><Match key={match.id} match={match} showUser={true} /></div>
