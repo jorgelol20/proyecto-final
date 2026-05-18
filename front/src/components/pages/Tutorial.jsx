acdae7a28d74b2fd2b874e1c4ee82104b5c2cd95
jorgelol20 <94186347+jorgelol20@users.noreply.github.com>
2026-05-18 20:44:59 +0200

    Tutorial

--- front/src/components/pages/Tutorial.jsx
+++ front/src/components/pages/Tutorial.jsx
@@ -20,9 +20,12 @@ import ShopManNormal from '/images/shopman/Normal.webp';
 import ShopManHappy from '/images/shopman/Happy.webp';
 import ShopManSarcastic from '/images/shopman/Sarcastic.webp';
 import ShopManThinking from '/images/shopman/Thinking.webp';
+import { useModifier } from "../../hooks/useModifier.js";
+import Modifier from "../Modifier.jsx";
 
 const Tutorial = () => {
     const navigate = useNavigate();
+    const { modifiers } = useModifier()
     const [currentIndex, setCurrentIndex] = useState(0);
     const { getTutorialCards } = useContext(matchContext);
 
@@ -39,13 +42,15 @@ const Tutorial = () => {
     const [defaultImage] = useImage(DefaultCardImage);
     const [hoveredIndex, setHoveredIndex] = useState(null);
 
+    // Tutorial 2
+    const modificadores = modifiers;
+
     // Escala para react-konva
     const [scale, setScale] = useState((window.innerWidth > window.innerHeight ? window.innerWidth : window.innerHeight) / 2560);
 
-    // 1. Cambiamos los componentes anidados por simples variables que almacenan JSX
     const slide1 = (
         <Fragment>
-            <div className="slide-1">
+            <div className="slide-1 slide">
                 <h1>Bienvenido a </h1>
                 <img src="/images/banner_menu.webp" alt="Banner Menu" />
                 <p>El roguelike estratégico de cartas más adictivo y dificil.</p>
@@ -55,7 +60,7 @@ const Tutorial = () => {
 
     const slide2 = (
         <Fragment>
-            <div className="slide-2">
+            <div className="slide-2 slide">
                 <div className="container">
                     <h1>Tu objetivo</h1>
                     <p>Te has adentrado en la mazmorra para conseguir <span>riquezas</span> y no tener que preocuparte más de ser pobre. ¿Sencillo verdad?</p>
@@ -68,7 +73,7 @@ const Tutorial = () => {
 
     const slide3 = (
         <Fragment>
-            <div className="slide-3">
+            <div className="slide-3 slide">
                 <div className="slide-3-info">
                     <h1>Cartas</h1>
                     <p>En <span>Scoundrel's Quest</span> cada carta tiene su función y motivo y la forma más fácil de conocerlas, es verlas.</p>
@@ -77,7 +82,7 @@ const Tutorial = () => {
                 <div className="card-stage">
                     {/* Carta 2 */}
                     <div key={tutorial1Cards[1]?.key + 3}>
-                        <Stage width={200 * scale} height={200 * scale} scaleX={scale} scaleX={scale} scaleY={scale} y={20*scale/20}
+                        <Stage width={200 * scale} height={200 * scale} scaleX={scale} scaleX={scale} scaleY={scale} y={20 * scale / 20}
                             onMouseOver={() => setHoveredIndex(tutorial1Cards[1]?.id)}
                             onMouseLeave={() => setHoveredIndex(null)}
                             onTap={() => hoveredIndex != null ? setHoveredIndex(null) : setHoveredIndex(tutorial1Cards[1]?.id)}
@@ -108,7 +113,7 @@ const Tutorial = () => {
 
                     {/* Carta 3 */}
                     <div key={tutorial1Cards[2]?.key + 3}>
-                        <Stage width={200 * scale} height={200 * scale} scaleX={scale} scaleY={scale} y={20*scale/20}
+                        <Stage width={200 * scale} height={200 * scale} scaleX={scale} scaleY={scale} y={20 * scale / 20}
                             onMouseOver={() => setHoveredIndex(tutorial1Cards[2]?.id)}
                             onMouseLeave={() => setHoveredIndex(null)}
                             onTap={() => hoveredIndex != null ? setHoveredIndex(null) : setHoveredIndex(tutorial1Cards[2]?.id)}
@@ -138,7 +143,7 @@ const Tutorial = () => {
 
                     {/* Carta 1 */}
                     <div key={tutorial1Cards[0]?.key + 3}>
-                        <Stage width={200 * scale} height={200 * scale} scaleX={scale} scaleX={scale} scaleY={scale} y={20*scale/20}
+                        <Stage width={200 * scale} height={200 * scale} scaleX={scale} scaleX={scale} scaleY={scale} y={20 * scale / 20}
                             onMouseOver={() => setHoveredIndex(tutorial1Cards[0]?.id)}
                             onMouseLeave={() => setHoveredIndex(null)}
                             onTap={() => hoveredIndex != null ? setHoveredIndex(null) : setHoveredIndex(tutorial1Cards[0]?.id)}
@@ -169,7 +174,7 @@ const Tutorial = () => {
 
                     {/* Carta 4 */}
                     <div key={tutorial1Cards[3]?.key + 3}>
-                        <Stage width={200 * scale} height={200 * scale} scaleX={scale}scaleX={scale} scaleY={scale} y={20*scale/20}
+                        <Stage width={200 * scale} height={200 * scale} scaleX={scale} scaleX={scale} scaleY={scale} y={20 * scale / 20}
                             onMouseOver={() => setHoveredIndex(tutorial1Cards[3]?.id)}
                             onMouseLeave={() => setHoveredIndex(null)}
                             onTap={() => hoveredIndex != null ? setHoveredIndex(null) : setHoveredIndex(tutorial1Cards[3]?.id)}
@@ -201,7 +206,66 @@ const Tutorial = () => {
         </Fragment>
     );
 
-    const slides = [slide1, slide2, slide3];
+
+    const slide4 = (
+        <Fragment>
+            <div className="slide-4 slide">
+                <h1>Modificadores</h1>
+                <p>En tu partida irás consiguiendo <span>modificadores</span> que te ayudarán a llegar lo más lejos que puedas.</p>
+                <p><span>Ponte encima</span> o <span>clica sobre ellos</span> para conocer sus efectos.</p>
+                <div className="tutorial-modifiers">
+                    {modifiers?.map(modifier => <Modifier modifierInfo={modifier} />)}
+                </div>
+            </div>
+        </Fragment>
+    )
+
+    const cardsEffects = [
+        { 'name': 'Anticuras', 'description': 'Durante el resto de la mano, te impide curarte.', 'image': '/images/cardEffects/Antiheal.webp', 'target': 'Enemigo' },
+        { 'name': 'Reducción de daño', 'description': 'Reduce el daño que sufres.', 'image': '/images/cardEffects/DmgReduction.webp', 'target': 'Curación' },
+        { 'name': 'Oro extra', 'description': 'El enemigo te da una cantidad de oro equivalente a su valor.', 'image': '/images/cardEffects/ExtraGold.webp', 'target': 'Enemigo' },
+        { 'name': 'Ruleta de curación', 'description': 'Hay una probabilidad de que te cure o te haga daño.', 'image': '/images/cardEffects/HealRoulete.webp', 'target': 'Curación' },
+        { 'name': 'Invencibilidad', 'description': 'No recibes daño directo del enemigo.', 'image': '/images/cardEffects/Invincibility.webp', 'target': 'Arma' },
+        { 'name': 'Saqueo', 'description': 'El enemigo te quita una cantidad de oro equivalente al doble de su valor.', 'image': '/images/cardEffects/Plunder.webp', 'target': 'Enemigo' },
+        { 'name': 'Veneno', 'description': 'Te va causando daño al pasar de mano durante una cantidad de manos fijas.', 'image': '/images/cardEffects/Poison.webp', 'target': 'Enemigo' },
+        { 'name': 'Curación progresiva', 'description': 'Te cura una cantidad de vida al pasar de mano durante una cantidad de manos fijas.', 'image': '/images/cardEffects/ProgresiveHeal.webp', 'target': 'Curación' },
+        { 'name': 'Restaurar habilidad', 'description': 'Restaura tu habilidad, pero no te cura.', 'image': '/images/cardEffects/RestoreAbility.webp', 'target': 'Curación' },
+        { 'name': 'Revivir', 'description': 'Si fueses a morir con golpeando con el arma portadora del efecto, sobrevives a 1 de vida.', 'image': '/images/cardEffects/Revive.webp', 'target': 'Arma' },
+        { 'name': 'Espinoso', 'description': 'Recibes 3 de daño fijo.', 'image': '/images/cardEffects/Thorny.webp', 'target': 'Enemigo' },
+        { 'name': 'Rompe Armas', 'description': 'Rompe el arma activa.', 'image': '/images/cardEffects/WeaponBreaker.webp', 'target': 'Enemigo' },
+    ]
+    const slide5 = (
+        <Fragment>
+            <div className="slide-5 slide">
+                <h1>Efectos en las cartas</h1>
+                <div style={{width: '80%'}}>
+                    <p>Aleatoriamente, algunas cartas enemigas podrán tener <span>efectos</span>. Para contrarrestar eso, en la tienda podrás obtener cartas (<span>curaciones</span> y <span>armas</span>) con efectos únicos.</p>
+                </div>
+                <div className="card-effects">
+                    {
+                        cardsEffects.map((effect, index) => {
+                            return <div key={index} className="card-effect">
+                                <div className="effect-image">
+                                    <img src={effect.image} alt={effect.name} />
+                                    <p><span>{effect.target}</span></p>
+                                </div>
+                                <div className="effect-text">
+                                    <div className="effect-name">
+                                        <h1>{effect.name}</h1>
+                                    </div>
+                                    <div className="effect-description">
+                                        <p>{effect.description}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        })
+                    }
+                </div>
+            </div>
+        </Fragment>
+    )
+
+    const slides = [slide1, slide2, slide3, slide4, slide5];
     const totalSlides = slides.length;
 
     const moveSlide = (direction) => {
@@ -222,6 +286,11 @@ const Tutorial = () => {
                 </div>
                 <button className="btn-prev" onClick={() => moveSlide(-1)}>&#10094;</button>
                 <button className="btn-next" onClick={() => moveSlide(1)}>&#10095;</button>
+                <div>
+                    {slides.map((slide, index)=>{
+
+                    })}
+                </div>
             </div>
         </Fragment>
     );
