5f89016352bd1bad48b69d3a1b715b8e1513cabf
jorgelol20 <94186347+jorgelol20@users.noreply.github.com>
2026-05-19 21:16:17 +0200

    Página de créditos

--- front/src/components/structure/Footer.jsx
+++ front/src/components/structure/Footer.jsx
@@ -1,11 +1,14 @@
 import React, { Fragment } from "react";
 import './Footer.css'
+import { NavLink } from "react-router-dom";
 const Footer = () => {
+    
     return (
         <Fragment>
             <footer>
                 <p>V.0.0 Alfa</p>
                 <a className="footer-link" href="mailto:soporte@scoundrels-quest.com">soporte@scoundrels-quest.com</a>
+                <NavLink to={"/creditos"}>Créditos</NavLink>
             </footer>
         </Fragment>
     )
