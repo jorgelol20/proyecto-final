15b801d29174a0500f6c2b0eba5b64c1318e27e8
jorgelol20 <94186347+jorgelol20@users.noreply.github.com>
2026-06-07 20:28:38 +0200

    Añadido placeholder logros

--- front/src/components/Achievement.jsx
+++ front/src/components/Achievement.jsx
@@ -1,12 +1,16 @@
 import React, { Fragment } from "react";
 import './Achievement.css';
+import Placeholder from 'images/achievement.webp'
 
 const Achievement = ({ achievementInfo, obtained = false }) => {
     return (
         <Fragment>
             <div className={obtained ? "achievement true" : "achievement false"}>
                 <div className="achievement-info">
-                    <img src={achievementInfo.icono} alt="" />
+                    <img src={achievementInfo.icono} alt=""
+                        onError={(e) => {
+                            e.currentTarget.src = Placeholder;
+                        }} />
                     <div>
                         <h1>{achievementInfo.nombre}</h1>
                         {achievementInfo.created_at ? <p>{new Date(achievementInfo.created_at).toLocaleDateString('es-ES')}</p> : <></>}
